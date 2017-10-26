5d84e02f6ebe7f6815a7a458552bac9b38054b9b
HideakiTouhara <hideee6723@gmail.com>
2017-10-26 22:36:00 +0900

    Implement changeProfileButton function

--- Happystagram/SettingViewController.swift
+++ Happystagram/SettingViewController.swift
@@ -8,28 +8,81 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var profileImageView: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usernameTextField.delegate = self
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: - UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            //backImage.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
-    */
+    
+    // アプリケーションロジック
+    
+    func openCamera() {
+        let sourceType = UIImagePickerControllerSourceType.camera
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let cameraPicker = UIImagePickerController()
+            cameraPicker.sourceType = sourceType
+            cameraPicker.delegate = self
+            present(cameraPicker, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func openPhoto() {
+        let sourceType = UIImagePickerControllerSourceType.photoLibrary
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let photoPicker = UIImagePickerController()
+            photoPicker.sourceType = sourceType
+            photoPicker.delegate = self
+            present(photoPicker, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - IBAction
+    
+    @IBAction func changeProfile(_ sender: UIButton) {
+        
+        let alertViewController = UIAlertController(title: "選択してください。", message: "", preferredStyle: .actionSheet)
+        alertViewController.addAction(UIAlertAction(title: "カメラ", style: .default, handler: { (action: UIAlertAction!) -> Void in
+            self.openCamera()
+            
+        }))
+        alertViewController.addAction(UIAlertAction(title: "アルバム", style: .default, handler: { (action: UIAlertAction!) -> Void in
+            self.openPhoto()
+            
+        }))
+        alertViewController.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
+        present(alertViewController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func logout(_ sender: UIButton) {
+    }
+    
+    @IBAction func back(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func done(_ sender: UIButton) {
+    }
+
 
 }
