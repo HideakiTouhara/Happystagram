05cf27db206da3cfbfaa4060a1e6a3e33ef497eb
HideakiTouhara <hideee6723@gmail.com>
2017-10-27 19:39:28 +0900

    Fetch profileImage and userName

--- Happystagram/EditViewController.swift
+++ Happystagram/EditViewController.swift
@@ -8,28 +8,39 @@
 
 import UIKit
 
-class EditViewController: UIViewController {
+class EditViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var myProfileImageView: UIImageView!
+    @IBOutlet weak var commentTextView: UITextView!
+    @IBOutlet weak var myProfileLabel: UILabel!
+    
+    var willEditImage = UIImage()
+    var userNameString = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageView.image = willEditImage
+        myProfileImageView.layer.cornerRadius = 8.0
+        myProfileImageView.clipsToBounds = true
+        commentTextView.delegate = self
+        
+        if UserDefaults.standard.object(forKey: "profileImage") != nil {
+            let decodeData = UserDefaults.standard.object(forKey: "profileImage")
+            let decodedData = NSData(base64Encoded: decodeData as! String, options: NSData.Base64DecodingOptions.ignoreUnknownCharacters)
+            let decodedImage = UIImage(data: decodedData! as Data)
+            myProfileImageView.image = decodedImage
+            userNameString = UserDefaults.standard.object(forKey: "userName") as! String
+            myProfileLabel.text = userNameString
+        } else {
+            myProfileImageView.image = UIImage(named: "logo.png")
+            myProfileLabel.text = "匿名"
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
